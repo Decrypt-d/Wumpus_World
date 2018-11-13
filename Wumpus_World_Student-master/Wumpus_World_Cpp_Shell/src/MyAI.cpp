2abc652532eb96624b6543c76be8930e8c220fcc
danhn255 <danhn255@gmail.com>
2018-11-12 18:10:00 -0800

    Greatly Improve Agent With Djikstra Algorithm

--- Wumpus_World_Student-master/Wumpus_World_Cpp_Shell/src/MyAI.cpp
+++ Wumpus_World_Student-master/Wumpus_World_Cpp_Shell/src/MyAI.cpp
@@ -42,10 +42,12 @@ MyAI::MyAI() : Agent()
     rightWall = -1;
 
     //backtracking info
-    backTrackingOn = false;
     turningAround = false;
     turningAroundComplete = false;
-    trail.push(pair<int,int>(1,1));
+
+    shouldClimbOut = false;
+    wumpusLocation.first = -1;
+    wumpusLocation.second = -1; 
 }
 
 void MyAI::handlePositionChange()
@@ -60,17 +62,61 @@ void MyAI::handlePositionChange()
         currentxValue -= 1;
 }
 
+
+void MyAI::removeAllUnexploredAtBoundary(const bool & removeFromRightWall)
+{
+    if (removeFromRightWall)
+    {
+        for (unordered_map<int,unordered_set<int>>::iterator beg = unexploredTile.begin(); beg != unexploredTile.end(); ++beg)
+            if ((*beg).first > rightWall)
+            {
+                unexploredTile.erase(beg);
+                return;
+            }
+    }
+    else
+    {
+        for (unordered_map<int,unordered_set<int>>::iterator beg = unexploredTile.begin(); beg != unexploredTile.end(); ++beg)
+        {
+            unordered_set<int>::iterator toRemove = (*beg).second.find(topWall + 1);
+            if (toRemove != (*beg).second.end())
+                (*beg).second.erase(toRemove);
+        }
+    }
+}
+
+
 void MyAI::handleBump()
 {
     if (orientation == EAST)
     {
+        
         currentxValue -= 1;
         rightWall = currentxValue;
+        //  std::cout << "Right Wall " << rightWall << std::endl; 
+        removeAllUnexploredAtBoundary(true);
     }
     else if (orientation == NORTH)
     {
         currentyValue -= 1;
         topWall = currentyValue;
+        //  std::cout << "Right Wall " << rightWall << std::endl; 
+        removeAllUnexploredAtBoundary(false);
+    }
+}
+
+void MyAI::handleWumpus(bool scream)
+{
+    if (shootWumpusX1Y1 && scream)
+    {
+        std::cout << "Wumpus Killed" << std::endl;
+        wumpusKilled = true;   
+    }
+    else if (shootWumpusX1Y1 && !scream)
+    {
+        std::cout << "Wumpus Alive" << std::endl;
+        wumpusLocation.first = 1;
+        wumpusLocation.second = 2;
     }
 }
 
@@ -105,21 +151,97 @@ vector<MyAI::direction> MyAI::determineWalls(int currentX, int currentY)
     if (currentY == topWall)
         removeFromVector(availableDirections, NORTH);
     
-    if (!backTrackingOn && trail.size() > 0)
-    {
-        pair<int,int> previousSquare = trail.top();
-        removeFromVector(availableDirections,determineBackTrackDirection(previousSquare.first,previousSquare.second));
-    }
-    
     return availableDirections;
 }
 
 bool MyAI::tileExist(const int &x, const int &y)
 {
-    map<int, std::map<int, tile>>::iterator iter = worldMap.find(x);
+    unordered_map<int, std::unordered_map<int, tile>>::iterator iter = worldMap.find(x);
     if (iter == worldMap.end()) return false;
-    map<int, tile>::iterator iter2 = worldMap[x].find(y);
+    unordered_map<int, tile>::iterator iter2 = worldMap[x].find(y);
     return iter2 != worldMap[x].end() ? true : false;
+}
+
+
+bool MyAI::allowTravelToTile(const int & x, const int & y)
+{
+    if (tileExist(x,y))
+        return true;
+    vector<pair<int,int>> tilesToCheck;
+    for (int i = -1; i < 2; i += 2)
+    {
+        // if (tileExist(x + i,y) && (!(wumpusLocation.first == x + i && wumpusLocation.second == y) || wumpusKilled))
+        if (tileExist(x + i,y))
+            tilesToCheck.push_back(pair<int,int>(x + i,y));
+        // if (tileExist(x, y + i) && (!(wumpusLocation.first == x && wumpusLocation.second == y + i) || wumpusKilled))
+        if (tileExist(x, y + i))
+            tilesToCheck.push_back(pair<int,int>(x,y + i));
+    }
+
+    for (int i = 0; i < tilesToCheck.size(); ++i)
+        if (!(worldMap[tilesToCheck[i].first][tilesToCheck[i].second].breeze || worldMap[tilesToCheck[i].first][tilesToCheck[i].second].stench))
+            return true;
+
+
+    if (tilesToCheck.size() > 1)
+    {
+        int breezeCount = 0;
+        int stenchCount = 0;
+        for (int i = 0; i < tilesToCheck.size(); ++i)
+        {
+            if (worldMap[tilesToCheck[i].first][tilesToCheck[i].second].breeze)
+                ++breezeCount;
+            if (worldMap[tilesToCheck[i].first][tilesToCheck[i].second].stench)
+                ++stenchCount;
+        }   
+        if (stenchCount < tilesToCheck.size() && breezeCount < tilesToCheck.size())
+            return true;
+    }
+    return false;
+}
+
+bool MyAI::posExceedBoundary(const int & x, const int & y)
+{
+    if (x < 1)
+        return true;
+    if (y < 1)
+        return true;
+    if (rightWall != - 1 && x > rightWall)
+        return true;
+    if (topWall != -1 && y > topWall)
+        return true;
+    return false;
+}
+
+void MyAI::removeFromUnexplored(const int & currentX,const int & currentY)
+{
+    unordered_map<int,unordered_set<int>>::iterator xPos = unexploredTile.find(currentX);
+    if (xPos != unexploredTile.end())
+    {
+        unordered_set<int>::iterator yPos = (*xPos).second.find(currentY);
+        if (yPos != (*xPos).second.end())
+            (*xPos).second.erase(yPos);
+        if ((*xPos).second.size() <= 0)
+            unexploredTile.erase(xPos);
+    }
+}
+
+void MyAI::addUnexploredTilePos(const int & currentX, const int & currentY)
+{
+    vector<pair<int,int>> unexploredPos;
+    for (int i = -1; i < 2; i += 2)
+    {
+        if (!(tileExist(currentX + i,currentY)))
+            if (!posExceedBoundary(currentX + i, currentY) && allowTravelToTile(currentX + i,currentY))
+                unexploredPos.push_back(pair<int,int>(currentX + i,currentY));
+        if (!(tileExist(currentX,currentY + i)))
+            if (!posExceedBoundary(currentX, currentY + i) && allowTravelToTile(currentX,currentY + i))
+                unexploredPos.push_back(pair<int,int>(currentX,currentY + i));
+    }
+    for (int i = 0; i < unexploredPos.size(); ++i)
+    {
+        unexploredTile[unexploredPos[i].first].insert(unexploredPos[i].second);  
+    }
 }
 
 void MyAI::addNewTile(bool glitter,bool stench,bool breeze)
@@ -131,6 +253,8 @@ void MyAI::addNewTile(bool glitter,bool stench,bool breeze)
         currentTile.stench = stench;
         currentTile.breeze = breeze;
         worldMap[currentxValue][currentyValue] = currentTile;
+        addUnexploredTilePos(currentxValue,currentyValue);
+        removeFromUnexplored(currentxValue,currentyValue);
     }
 }
 
@@ -183,34 +307,7 @@ void MyAI::createSequenceOfAction(const MyAI::direction & chosenDirection)
        sequenceOfActions.push(action);  
        mockOrientation = resolveNewOrientation(mockOrientation, action);
     }
-    sequenceOfActions.push(FORWARD);
-}
-
-Agent::Action MyAI::Turn_Left(){
-    orientation = resolveNewOrientation(orientation,TURN_LEFT);
-    return TURN_LEFT;
-}
-
-Agent::Action MyAI::Turn_Right(){
-    orientation = resolveNewOrientation(orientation,TURN_RIGHT);
-    return TURN_RIGHT;
-}
-
-Agent::Action MyAI::Forward(){
-    if (!backTrackingOn)
-        trail.push(pair<int,int>(currentxValue,currentyValue));
-    handlePositionChange();
-    return FORWARD;
-}
-
-Agent::Action MyAI::resolveAction(Agent::Action action)
-{
-    if (action == TURN_LEFT)
-        return Turn_Left();
-    else if (action == TURN_RIGHT)
-        return Turn_Right();
-    else
-        return Forward();    
+    // sequenceOfActions.push(FORWARD);
 }
 
 MyAI::direction MyAI::determineBackTrackDirection(const int & destinationxValue,const int & destinationyValue)
@@ -231,33 +328,257 @@ MyAI::direction MyAI::determineBackTrackDirection(const int & destinationxValue,
     }
 }
 
-void MyAI::backtrackAction()
-{
-   pair<int,int> destinationTile = trail.top();
-   trail.pop();
-   MyAI::direction chosenBackTrackDir = determineBackTrackDirection(destinationTile.first,destinationTile.second);
-//    std::cout << "Chosen BkTrack Dir " << chosenBackTrackDir << std::endl; 
-   createSequenceOfAction(chosenBackTrackDir);  
+Agent::Action MyAI::Turn_Left(){
+    orientation = resolveNewOrientation(orientation,TURN_LEFT);
+    return TURN_LEFT;
 }
+
+Agent::Action MyAI::Turn_Right(){
+    orientation = resolveNewOrientation(orientation,TURN_RIGHT);
+    return TURN_RIGHT;
+}
+
+Agent::Action MyAI::Forward(){
+    handlePositionChange();
+    return FORWARD;
+}
+
+Agent::Action MyAI::resolveAction(Agent::Action action)
+{
+    if (action == TURN_LEFT)
+        return Turn_Left();
+    else if (action == TURN_RIGHT)
+        return Turn_Right();
+    else
+        return Forward();    
+}
+
+//Djikstra for next path ---------------------
+
+void MyAI::updateActionNodeCost(actionNode & toUpdate)
+{
+    if (toUpdate.previousState == nullptr)
+        toUpdate.cost = 0;
+    else
+        toUpdate.cost = (toUpdate.previousState)->cost + (toUpdate.actionToGetHere == AROUND_MOVE ? 2 : 1 );
+}
+
+void MyAI::resolveActionNodeOrientation(actionNode & toUpdate,const MyAI::direction & curOrientation)
+{
+    if (toUpdate.actionToGetHere == AROUND_MOVE)
+        toUpdate.currentOrientation = (MyAI::direction) ((curOrientation + 2) % 4); 
+    else if (toUpdate.actionToGetHere == LEFT_MOVE)
+        toUpdate.currentOrientation = resolveNewOrientation(curOrientation, TURN_LEFT);
+    else if (toUpdate.actionToGetHere == RIGHT_MOVE)
+        toUpdate.currentOrientation = resolveNewOrientation(curOrientation, TURN_RIGHT);
+}
+
+
+pair<int,int> MyAI::determinePositionAfterForward(const int & currentX, const int & currentY, const MyAI::direction & curOrientation)
+{
+    pair<int,int> result;
+    result.first = currentX;
+    result.second = currentY;
+    if ( curOrientation == NORTH)
+        result.second += 1;
+    else if ( curOrientation == SOUTH)
+        result.second -= 1;
+    else if ( curOrientation == EAST)
+        result.first += 1;
+    else if ( curOrientation == WEST)
+        result.first -= 1;
+    return result;
+}
+
+
+template <typename T>
+bool locationExistInVector(const vector<T*> & vecToCheck,const pair<int,int> & location)
+{
+    for (typename vector<T*>::const_iterator beg = vecToCheck.begin(); beg != vecToCheck.end(); ++beg)
+    {
+        if ((*(*beg)).location.first == location.first && (*(*beg)).location.second == location.second)
+            return true; 
+    }
+    return false;
+}
+
+void MyAI::generateNextActionNodes(std::priority_queue<actionNode * ,std::vector<actionNode *>,actionNode::actionNodeComparitor> & pq,vector<actionNode * > & exploredSet, vector<actionNode * > & currentNodes, const actionNode & previousState)
+{
+    if (previousState.allowTurn)
+    {
+        for (int i = 0; i < 3; ++i)
+        {
+            actionNode * newNode = new actionNode();
+            (*newNode).location = previousState.location;
+            (*newNode).actionToGetHere = (MyAI::movement) i;
+            (*newNode).previousState = &previousState;
+            (*newNode).allowTurn = false; 
+            updateActionNodeCost(*newNode);
+            resolveActionNodeOrientation(*newNode,previousState.currentOrientation);
+            currentNodes.push_back(newNode);
+            pq.push(newNode);
+        }
+    }
+    pair<int,int> newLocIfForward = determinePositionAfterForward(previousState.location.first,previousState.location.second,previousState.currentOrientation);
+
+    if (!locationExistInVector<actionNode>(exploredSet,newLocIfForward))
+        if (!posExceedBoundary(newLocIfForward.first, newLocIfForward.second) && allowTravelToTile(newLocIfForward.first,newLocIfForward.second))
+        {
+           
+            actionNode * newNode = new actionNode();
+            (*newNode).location = newLocIfForward;
+            (*newNode).actionToGetHere = FORWARD_MOVE;
+            (*newNode).previousState = &previousState;
+            (*newNode).allowTurn = true; 
+            (*newNode).currentOrientation = previousState.currentOrientation;
+            updateActionNodeCost(*newNode);
+            currentNodes.push_back(newNode);
+            pq.push(newNode);
+            exploredSet.push_back(newNode);
+        }
+}
+
+template <typename T>
+void cleanUpPointersVector(vector<T> & toCleanUp)
+{
+    for (typename vector<T>::iterator beg = toCleanUp.begin(); beg != toCleanUp.end(); ++beg)
+        delete (*beg);
+} 
+
+template <typename T>
+std::ostream & operator<< (std::ostream & out, const vector<T> & toPrint)
+{
+    for (typename vector<T>::const_iterator beg = toPrint.begin(); beg != toPrint.end(); ++beg)
+        std::cout << (*beg) << std::endl; 
+    return out;
+}
+
+void MyAI::reconstructPath(std::stack<Agent::Action> & actionStack,const actionNode * source)
+{
+    const actionNode * currentNode = source;
+    while (currentNode->previousState != nullptr)
+    {
+        if (currentNode->actionToGetHere == FORWARD_MOVE)
+            actionStack.push(FORWARD);
+        else if (currentNode->actionToGetHere == LEFT_MOVE)
+            actionStack.push(TURN_LEFT);
+        else if (currentNode->actionToGetHere == RIGHT_MOVE)
+            actionStack.push(TURN_RIGHT);
+        else if (currentNode->actionToGetHere == AROUND_MOVE)
+        {
+            actionStack.push(TURN_LEFT);
+            actionStack.push(TURN_LEFT);
+        }
+        currentNode = currentNode->previousState;
+    }
+
+    int size = actionStack.size();
+    for (int i = 0; i < size; ++i)
+    {
+        sequenceOfActions.push(actionStack.top());
+        actionStack.pop();
+    }
+}
+
+bool MyAI::findNextPath(const bool & shouldReturnToStart)
+{
+    actionNode * source = new actionNode();
+    (*source).currentOrientation = orientation;
+    (*source).location = pair<int,int>(currentxValue,currentyValue);
+    (*source).cost = 0;
+    vector<actionNode *> exploredSet;
+    vector<actionNode *> currentNodes;
+    std::priority_queue<actionNode * ,std::vector<actionNode *>,actionNode::actionNodeComparitor> pq;
+    pq.push(source);
+    exploredSet.push_back(source);
+    currentNodes.push_back(source);
+    actionNode * topNode = pq.top();
+    if (!shouldReturnToStart)
+    {
+        while (!pq.empty())
+        {
+            topNode = pq.top();
+            pq.pop();
+            if (!(tileExist(topNode->location.first,topNode->location.second)))
+                break;
+            generateNextActionNodes(pq,exploredSet,currentNodes,*topNode);
+        }
+    }
+    else
+    {
+        while (topNode->location.first != 1 || topNode->location.second != 1)
+        {
+            topNode = pq.top();
+            pq.pop();
+            generateNextActionNodes(pq,exploredSet,currentNodes,*topNode);
+        }
+    }
+
+    // std::cout << currentNodes << std::endl;
+    // std::cout << "---------------BEST NODES------------------" << std::endl;
+    // std::cout << topNode << std::endl;
+    std::stack<Agent::Action> actionStack;
+    reconstructPath(actionStack,topNode);
+    cleanUpPointersVector(currentNodes);
+    return true;
+}
+
+//--------------------------
+
 
 //main method
 Agent::Action MyAI::getAction(bool stench, bool breeze, bool glitter, bool bump, bool scream)
 {
-    //adding tile
-    addNewTile(glitter,stench,breeze);
-    tile currentTile = worldMap[currentxValue][currentyValue];
-    
     //updates wall location information
     if (bump)
         handleBump();
 
-    //***************************************************Action Logic***************************************************
+    // handleWumpus(scream);
+    //adding tile
+    addNewTile(glitter,stench,breeze);
+    tile currentTile = worldMap[currentxValue][currentyValue];
+
+    // std::cout << "Current X Y " << currentxValue << " " << currentyValue << std::endl;
+ 
+    int unexploredSize = 0;
+    for (unordered_map<int,unordered_set<int>>::iterator beg = unexploredTile.begin(); beg != unexploredTile.end(); ++beg)
+        for (unordered_set<int>::iterator beg2 = ((*beg).second).begin(); beg2 != ((*beg).second).end(); ++beg2)
+        {
+            //  std::cout << "Unexplored Pos: " << ((*beg).first) << " , " << (*beg2) << std::endl;
+             unexploredSize += ((*beg).second).size();
+        }
     
+    // std::cout << "Unexplored Size " << unexploredSize << std::endl;
+  
+
+
+    //***************************************************Action Logic***************************************************
+    if((currentxValue == 1 && currentyValue == 1) && (breeze ||  goldGrabbed || shouldClimbOut || stench))
+       return CLIMB;
+
+    // if ((currentxValue == 1 && currentyValue == 1) && stench && (!wumpusKilled && wumpusLocation.first == -1 && wumpusLocation.second == -1))
+    // {
+    //     shootWumpusX1Y1 = true;
+    //     return SHOOT;
+    // }
+       
     if (currentTile.glitter && !goldGrabbed)
     {
         goldGrabbed = true;
         return GRAB;
     }
+
+    if (unexploredSize <= 0 || goldGrabbed){
+        findNextPath(true);
+        if (sequenceOfActions.size() != 0)
+        {
+            shouldClimbOut = true;
+            Agent::Action action = sequenceOfActions.front();
+            sequenceOfActions.pop();
+            return resolveAction(action);
+        }
+    }
+
 
     //continues to resolve desired direction if needed
     if (sequenceOfActions.size() != 0)
@@ -266,59 +587,28 @@ Agent::Action MyAI::getAction(bool stench, bool breeze, bool glitter, bool bump,
         sequenceOfActions.pop();
        return resolveAction(action);
     }  
+    
+    if (unexploredTile.size() > 0)
+    {
+        findNextPath(false);
+        if (sequenceOfActions.size() != 0)
+        {
+            Agent::Action action = sequenceOfActions.front();
+            sequenceOfActions.pop();
+            return resolveAction(action);
+        }
+    }  
 
 
-    if((currentxValue == 1 && currentyValue == 1) && (breeze = true ||  goldGrabbed || no unexplored edges))
-       return CLIMB
-    
-    if(currentxValue == 1 && currentyValue == 1 && stench == true)
-       -shoot and move in appropriate direction
-    
-    if(breeze || stench)
-       -get all possible directions
-       -Check if that tile that we are going to has an adjacent tile that is explored and is safe
-        if (it has an adjacent tile that is explored and is safe, we can go to that tile)
-   
-   if (safe)
-     -forward
-   else
-     Backtrack to previous tile. - remove the direction which we came from when we backtrack
-     Djikstra for backtracking
 }
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//need function for no unexplored edges
+//need function to check if tile is safe
+//need function to check adjacent tiles
+//need backtracking + djsktra
 
 
 /*
@@ -368,3 +658,23 @@ Agent::Action MyAI::getAction(bool stench, bool breeze, bool glitter, bool bump,
         sequenceOfActions.pop();
        return resolveAction(action);
     }  */
+
+
+
+
+//     if((currentxValue == 1 && currentyValue == 1) && (breeze = true ||  goldGrabbed || no unexplored edges))
+//        return CLIMB
+    
+//     if(currentxValue == 1 && currentyValue == 1 && stench == true)
+//        -shoot and move in appropriate direction
+    
+//     if(breeze || stench)
+//        -get all possible directions
+//        -Check if that tile that we are going to has an adjacent tile that is explored and is safe
+//         if (it has an adjacent tile that is explored and is safe, we can go to that tile)
+   
+//    if (safe and unexplored)
+//      -forward
+//    else
+//      Backtrack to previous tile. - remove the direction which we came from when we backtrack
+//      Djikstra for backtracking
--- Wumpus_World_Student-master/Wumpus_World_Cpp_Shell/src/MyAI.hpp
+++ Wumpus_World_Student-master/Wumpus_World_Cpp_Shell/src/MyAI.hpp
@@ -21,11 +21,12 @@
 #define MYAI_LOCK
 
 #include "Agent.hpp"
-#include <map>
+#include <unordered_map>
 #include <stack> 
 #include <queue>
 #include <vector>
 #include <iostream>
+#include <unordered_set>
 
 
 using namespace std;
@@ -37,6 +38,7 @@ public:
 
 	//boundary and wall variables
 	enum direction {NORTH,EAST,SOUTH,WEST};
+	enum movement {LEFT_MOVE, RIGHT_MOVE, AROUND_MOVE, FORWARD_MOVE };
 	int topWall;
 	int rightWall;
 
@@ -46,16 +48,18 @@ public:
 	int currentScore;
 	bool wumpusKilled;
 	bool goldGrabbed;
+	bool shouldClimbOut;
+	pair<int,int> wumpusLocation;
+	bool shootWumpusX1Y1 = false;
 
 	//position info
 	MyAI::direction orientation; 
 	int currentxValue;
 	int currentyValue;
 
-	//backtracking functionality
-	stack<pair<int,int>> trail;
+	//backtracking and exploring functionality
+	unordered_map<int,unordered_set<int>> unexploredTile;
 	queue<Agent::Action> sequenceOfActions;
-	bool backTrackingOn;
 	bool turningAround;
 	bool turningAroundComplete;
 	
@@ -85,8 +89,43 @@ public:
 		   }
 	};
 
+	struct actionNode
+	{
+		const actionNode * previousState = nullptr;
+		MyAI::direction currentOrientation;
+		int cost;
+		pair<int,int> location;
+		bool allowTurn = true;
+		MyAI::movement actionToGetHere;
+
+		friend std::ostream & operator<<(std::ostream & out, actionNode * s)
+		{
+			operator << (out, *s);
+		}
+		friend std::ostream & operator<<(std::ostream & out, const actionNode & s)
+		{
+			std::cout << "Previous Node Address " << s.previousState << std::endl;
+			std::cout << "Current Orientation " << s.currentOrientation << std::endl;
+			std::cout << "Cost " << s.cost << std::endl;
+			std::cout << "Location " << s.location.first << " , " << s.location.second << std::endl;
+			std::cout << "Allow Turn " << s.allowTurn << std::endl;
+			std::cout << "Action To Get Here " << s.actionToGetHere << std::endl;
+			return out;
+		}
+
+
+
+		struct actionNodeComparitor
+		{
+			bool operator()(const actionNode * const & lhs, const actionNode * const & rhs)
+			{
+				return lhs->cost > rhs->cost;
+			}
+		};
+	};
+
 	//vector that will contain all tiles
-	map<int,map<int,tile>> worldMap;
+	unordered_map<int,unordered_map<int,tile>> worldMap;
 	
 	//main method
 	Action getAction(bool stench, bool breeze, bool glitter, bool bump, bool scream);
@@ -95,20 +134,31 @@ public:
 	void handlePositionChange();
     vector<MyAI::direction> determineWalls(int currentX, int currrentY);
 	Agent::Action adjustDirection(const MyAI::direction & orientation,const MyAI::direction & chosenDirection);
-	void backtrackAction();
 	MyAI::direction determineBackTrackDirection(const int & destinationxValue,const int & destinationyValue);
 	MyAI::direction resolveNewOrientation(const MyAI::direction & orientation,const Agent::Action & action);
-	
+	bool allowTravelToTile(const int & X, const int & y);
 	Agent::Action Forward();
 	Agent::Action Turn_Left();
 	Agent::Action Turn_Right();
 	void createSequenceOfAction(const MyAI::direction & chosenDirection);
 	Agent::Action resolveAction(Agent::Action action);
+	bool posExceedBoundary(const int & x, const int & y);
+	bool findNextPath(const bool & shouldReturnToStart);
+	void generateNextActionNodes(std::priority_queue<actionNode *,std::vector<actionNode *>,actionNode::actionNodeComparitor> & pq,vector<actionNode * > & exploredSet, vector<actionNode * > & currentNode, const actionNode & previousState);
+	pair<int,int> determinePositionAfterForward(const int & currentX, const int & currentY, const MyAI::direction & curOrientation);
+	void updateActionNodeCost(actionNode & toUpdate);
+	void resolveActionNodeOrientation(actionNode & toUpdate,const MyAI::direction & curOrientation);
+	void reconstructPath(std::stack<Agent::Action> & actionStack,const actionNode * source);
+	void handleWumpus(bool scream);
+
 
 	//tile related methods
     bool tileExist(const int & x,const int & y);
     void addNewTile(bool glitter, bool stench, bool breeze);
 	void handleBump();
+	void addUnexploredTilePos(const int & currentX, const int & currentY);
+	void removeFromUnexplored(const int & currentX,const int & currentY);
+	void removeAllUnexploredAtBoundary(const bool & removeFromRightWall);
 };
 
 #endif
